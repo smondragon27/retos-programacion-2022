1f208cb59d916345613052a324c0f3b4e4b57ce4
smondragon <smondragon2799@gmail.com>
2024-01-22 22:19:03 -0500

    se sube reto 02 finalizado

--- retos_1-5/reto_02.py
+++ retos_1-5/reto_02.py
@@ -6,3 +6,34 @@
 #  * - NO hace falta comprobar que ambas palabras existan.
 #  * - Dos palabras exactamente iguales no son anagrama.
 #  */
+
+
+
+
+def is_anagrama(palabra1,palabra2):
+
+    palabra1 = palabra1.lower()
+    palabra2 = palabra2.lower()
+
+    arr_palabra1 = list(palabra1)
+    arr_palabra2 = list(palabra2)
+
+    arr_palabra1.sort()
+    arr_palabra2.sort()
+
+    palabra1 = "".join(arr_palabra1)
+    palabra2 = "".join(arr_palabra2)
+
+    if palabra1 == palabra2:
+        return True
+    else:
+        return False
+   
+
+
+resultado = is_anagrama("Nacionalistsa","Altisonancia")
+
+if resultado:
+    print("Es anagrama")
+else:
+    print("No es anagrama")
